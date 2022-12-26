8862bbb0337a3e519d3ec2055d98dbe4f1f66b80
TheTeamInsane <115443754+TheTeamInsane@users.noreply.github.com>
2022-12-26 15:07:37 +0530

    Update settings.py

--- InsaneMusic/utils/inline/settings.py
+++ InsaneMusic/utils/inline/settings.py
@@ -25,7 +25,7 @@ def setting_markup(_):
         [
             InlineKeyboardButton(
                 text="• Sᴜᴩᴩᴏʀᴛ •",
-                url="https://t.me/Shayri_Music_Lovers",
+                url="https://t.me/Dosto_ki_Mehfil786",
             ),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
