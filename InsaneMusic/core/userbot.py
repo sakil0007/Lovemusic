254a9ee09a0b631cf5beb54dfc004b01cc13dcdf
AYRA-XD143 <122684250+AYRA-XD143@users.noreply.github.com>
2023-02-23 06:28:20 +0530

    Add files via upload

--- InsaneMusic/core/userbot.py
+++ InsaneMusic/core/userbot.py
@@ -53,7 +53,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Dosto_ki_Mehfil786")
+                await self.one.join_chat("Secret_x_chatting")
             except:
                 pass
             assistants.append(1)
@@ -79,7 +79,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Dosto_ki_Mehfil786")
+                await self.two.join_chat("Secret_x_chatting")
             except:
                 pass
             assistants.append(2)
@@ -105,7 +105,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Dosto_ki_Mehfil786")
+                await self.three.join_chat("Secret_x_chatting")
             except:
                 pass
             assistants.append(3)
@@ -131,7 +131,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Dosto_ki_Mehfil786")
+                await self.four.join_chat("Secret_x_chatting")
             except:
                 pass
             assistants.append(4)
@@ -157,7 +157,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Dosto_ki_Mehfil786")
+                await self.five.join_chat("Secret_x_chatting")
             except:
                 pass
             assistants.append(5)
